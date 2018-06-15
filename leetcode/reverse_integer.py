605ef68546e5fc139015ff31cde806a5699efb55
markliu2013 <markliu2013@gmail.com>
2018-06-15 08:54:52 +0800

    reverse int

--- leetcode/reverse_integer.py
+++ leetcode/reverse_integer.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Solution:
     @staticmethod
     def reverse(x):
@@ -7,4 +10,23 @@ class Solution:
             result = int(str(x)[::-1])
         return 0 if result > (2**31 - 1) or result < -(2**31) else result
 
-print(Solution.reverse(-2147483648))
+    @staticmethod
+    def reverse2(x):
+        x1 = list(str(abs(x)))
+        x1.reverse()
+        result = int(''.join(x1)) * (-1 if x < 0 else 1)
+        return 0 if result > (2 ** 31 - 1) or result < -(2 ** 31) else result
+
+    @staticmethod
+    def reverse3(x):
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        result = 0
+        while x > 0:
+            reminder = x % 10
+            x = int(x / 10)
+            result = result * 10 + reminder
+        result = result * sign
+        return 0 if result > (2 ** 31 - 1) or result < -(2 ** 31) else result
+
+print(Solution.reverse3(-1234))
